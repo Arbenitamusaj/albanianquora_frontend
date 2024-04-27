3030412b1b1dd36a5e25f1f41a40a635484bc8be
ShqiperimRamadani <166860851+ShqiperimRamadani@users.noreply.github.com>
2024-04-27 07:25:04 +0200

    Added yamal files and .env file

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,13 +1,13 @@
-import NavBar from '../../components/NavBar';
-import React, { useState, useContext } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import { AuthContext } from '../../../context/AuthContext'; 
+import NavBar from "../../components/NavBar";
+import React, { useState, useContext } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { AuthContext } from "../../../context/AuthContext";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login } = useContext(AuthContext);
   const router = useRouter();
 
@@ -15,21 +15,24 @@ export default function Login() {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5274/auth/login', {
+      const response = await axios.post("DB_HOST:5274/auth/login", {
         email,
-        password
+        password,
       });
 
-      console.log('Login Successful:', response.data);
-      login(response.data.token); 
-      router.push('/home');
+      console.log("Login Successful:", response.data);
+      login(response.data.token);
+      router.push("/home");
     } catch (error) {
-      console.error('Login failed:', error.response ? error.response.data : error.message);
+      console.error(
+        "Login failed:",
+        error.response ? error.response.data : error.message
+      );
     }
   };
 
   return (
-    <div className='w-full h-screen bg-gray-200'>
+    <div className="w-full h-screen bg-gray-200">
       <div className="flex flex-col items-center text-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
         <div className="w-full bg-white text-black rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -37,8 +40,13 @@ export default function Login() {
               Sign in to your account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div className='text-left'>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+              <div className="text-left">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -50,8 +58,13 @@ export default function Login() {
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div className='text-left'>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+              <div className="text-left">
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
                 <input
                   type="password"
                   name="password"
@@ -63,9 +76,20 @@ export default function Login() {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <button type="submit" className="w-full text-black border-gray-900 bg-white hover:bg-gray-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 border hover:text-white">Sign in</button>
+              <button
+                type="submit"
+                className="w-full text-black border-gray-900 bg-white hover:bg-gray-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 border hover:text-white"
+              >
+                Sign in
+              </button>
               <p className="text-sm font-light">
-                Don’t have an account yet? <Link href="/auth/register" className="font-medium text-gray-600 hover:underline">Sign up</Link>
+                Don’t have an account yet?{" "}
+                <Link
+                  href="/auth/register"
+                  className="font-medium text-gray-600 hover:underline"
+                >
+                  Sign up
+                </Link>
               </p>
             </form>
           </div>
